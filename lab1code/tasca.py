5b35de8cf7eeee172eed9b1106656ee917862e82
Leo <1707505@uab.cat>
2026-02-19 11:48:39 +0100

    Define functions for graph construction and traversal
    Added function definitions for graph tasks.

--- lab1code/tasca.py
+++ lab1code/tasca.py
@@ -1,1 +1,13 @@
+# Tasca 1: Construir el graf
+def build_graph():
 
+# Tasca 2: Components BFS
+
+def components_BFS(G):
+
+# Tasca 3: Components DFS
+def components_DFS(G):
+
+
+# Tasca 4: Experiment tallant arestes
+def experiment_cut_edges():
