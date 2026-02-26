07e2227e3f0487cc867ac1c8671af262c3f0f975
1728365l <1728365@uab.cat>
2026-02-26 12:05:09 +0100

    BFS_PRAC1_primera_versió.py

--- lab1code/lab1.py
+++ lab1code/lab1.py
@@ -1,9 +1,58 @@
-# Tasca 1: Construir el graf
-def build_graph():
+import networkx as nx
 
+# Tasca 1: Construir el graf
+def build_graph(nom_arxiu):
+  G=nx.graph()
+  try:
+    arxiu=open(nom_arxiu, 'r')
+  
+  except FileNotFoundError:
+    raise FileNotFoundError
+  
+  else:
+    arxiu.readline()
+    for line in arxiu:
+      Aresta=list(line.split(','))
+      G.add_edge(Aresta)
+    return G
+    
 # Tasca 2: Components BFS
 
 def components_BFS(G):
+  Nodes_tots=[]
+  Nodes_revisar=[]
+  Llista_revisats=[]
+  Llista_components=[]
+  for node in G.nodes():
+    Nodes_tots.append(node)
+  Node_actual=Nodes_tots[0]
+  Llista_components.append([Node_actual])
+  Buit=False
+  Revisar=True
+  Component=1
+  while not Buit:
+    while Revisar:
+      for node in all_neighbors(Node_actual)
+        if node not in Llista_revisats:
+          Nodes_revisar.append()
+      
+      Llista_revisats.append(Node_actual)
+      Nodes_tots.pop(Node_actual)
+      
+      if len(Llista_components)<Component:
+        Llista_components.append([Node_actual])
+      else:
+        Llista_components[Component].append(Node_actual)
+
+      Nodes_revisar=Nodes_revisar[1:]
+      if Nodes_revisar == []:
+        Revisar=False
+      else:
+        Node_actual=Nodes_revisar[0]
+    if Nodes_tots !=[]:
+      Node_actual=Nodes_tots[0]
+    else:
+      Buit=True
 
 # Tasca 3: Components DFS
 def components_DFS(G):
